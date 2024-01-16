01ce4be3b72dc98ccb18a3a4771ce219ff6bf9ec
Mohamed <mmahmoud.c@saudivts.com>
2024-01-16 14:28:40 +0300

    editing the main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy
 print("Hello, Docker!")
-arr = numpy.array([1, 2, 3, 4, 5])
+arr = numpy.array([1, 2, 3, 4, 5,5,6,8,56,98])
 
-print(f"this is a numpy array, {arr}")
+print(f"this is a numpy array,  {arr}")
+print(f"this is a numpy array,  {arr.avg()}")
+
       
